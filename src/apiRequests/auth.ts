17977e8165b8ff43fe1ae507f962f97e9814b865
BinhHung <binhhung2399@gmail.com>
2024-07-29 16:35:06 +0700

    Code route handler logout

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,5 +1,9 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+} from "@/schemaValidations/auth.schema";
 
 const authApiRequests = {
   sLogin: (body: LoginBodyType) => http.post<LoginResType>("/auth/login", body),
@@ -7,6 +11,19 @@ const authApiRequests = {
     http.post<LoginResType>("/api/auth/login", body, {
       baseUrl: "",
     }),
+  sLogout: (body: LogoutBodyType & { accessToken: string }) =>
+    http.post(
+      "/auth/logout",
+      {
+        refreshToken: body.refreshToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      }
+    ),
+  cLogout: () => http.post("/api/auth/logout", null, { baseUrl: "" }),
 };
 
 export default authApiRequests;
