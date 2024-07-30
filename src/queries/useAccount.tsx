2d2406a3f8d70a7a53bfd736db9d545aa0c29f85
BinhHung <binhhung2399@gmail.com>
2024-07-30 14:45:03 +0700

    Hiển thị thông tin profile và preview avatar

--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,9 +1,16 @@
 import accountApiRequests from "@/apiRequests/account";
+import { AccountResType } from "@/schemaValidations/account.schema";
 import { useQuery } from "@tanstack/react-query";
 
-export const useAccountProfile = () => {
+export const useAccountProfile = (
+  onSuccess?: (data: AccountResType) => void
+) => {
   return useQuery({
     queryKey: ["profile"],
-    queryFn: accountApiRequests.me,
+    queryFn: () =>
+      accountApiRequests.me().then((res) => {
+        if (onSuccess) onSuccess(res.payload);
+        return res;
+      }),
   });
 };
