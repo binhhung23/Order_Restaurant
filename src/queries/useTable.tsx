37119dcc2738158e6b77046c7cd6a615ac121db4
BinhHung <binhung2311@gmail.com>
2024-08-30 14:48:35 +0700

    Khai báo API cho quản lý bàn ăn

---
+++ src/queries/useTable.tsx
@@ -0,0 +1,64 @@
+import tableApiRequests from "@/apiRequests/table";
+import { UpdateTableBodyType } from "@/schemaValidations/table.schema";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useGetDishList = () => {
+  return useQuery({
+    queryKey: ["tables"],
+    queryFn: tableApiRequests.getListTable,
+  });
+};
+
+export const useGetDish = ({
+  number,
+  enabled,
+}: {
+  number: number;
+  enabled: boolean;
+}) => {
+  return useQuery({
+    queryKey: ["table", number],
+    queryFn: () => tableApiRequests.getTable(number),
+    enabled,
+  });
+};
+
+export const useAddTableMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: tableApiRequests.addTable,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["tables"],
+      });
+    },
+  });
+};
+
+export const useUpdateTableMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({
+      number,
+      ...body
+    }: { number: number } & UpdateTableBodyType) =>
+      tableApiRequests.updateTable(number, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["tables"],
+      });
+    },
+  });
+};
+
+export const useDeleteTableMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: tableApiRequests.deleteTable,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["tables"],
+      });
+    },
+  });
+};
