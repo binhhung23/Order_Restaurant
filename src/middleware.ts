57a2f7eee979ba9637f8a5b05c6b6efa55dbaabe
BinhHung <binhung2311@gmail.com>
2024-09-09 15:56:46 +0700

    Xử lý phân quyền giữa khách và quản lý

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,57 @@
+import { Role } from "@/constants/type";
+import { decodeToken } from "@/lib/utils";
 import { NextResponse, NextRequest } from "next/server";
 
-const privatePaths = ["/manage"];
+const managePaths = ["/manage"];
+const guestPaths = ["/guest"];
+const privatePaths = [...managePaths, ...guestPaths];
 const unAuthPaths = ["/login"];
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
+  //Trường hợp chưa đăng nhập ko cho vào private path
   if (privatePaths.some((path) => pathname.startsWith(path)) && !refreshToken) {
     const url = new URL("/login", request.url);
     url.searchParams.set("clearToken", "true");
     return NextResponse.redirect(url);
   }
 
-  if (unAuthPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  //Trường hợp đăng nhập rồi
+  if (refreshToken) {
+    //mà cố vào trang login thì cho về trang chủ
+    if (unAuthPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    //mà acceston ken het han
+    if (
+      privatePaths.some((path) => pathname.startsWith(path)) &&
+      !accessToken
+    ) {
+      const url = new URL("/refresh-token", request.url);
+      url.searchParams.set("refreshToken", refreshToken);
+      url.searchParams.set("redirect", pathname);
+      return NextResponse.redirect(url);
+    }
 
-  if (
-    privatePaths.some((path) => pathname.startsWith(path)) &&
-    !accessToken &&
-    refreshToken
-  ) {
-    const url = new URL("/refresh-token", request.url);
-    url.searchParams.set("refreshToken", refreshToken);
-    url.searchParams.set("redirect", pathname);
-    return NextResponse.redirect(url);
-  }
+    // Vào ko đúng route, redirect về trang chủ
+    const role = decodeToken(refreshToken).role;
+    const isGuestGoToManagePath =
+      role === Role.Guest &&
+      managePaths.some((path) => pathname.startsWith(path));
+    const isNotGuestGoToGuestPath =
+      role !== Role.Guest &&
+      guestPaths.some((path) => pathname.startsWith(path));
 
-  return NextResponse.next();
+    if (isGuestGoToManagePath || isNotGuestGoToGuestPath) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return NextResponse.next();
+  }
 }
 
 export const config = {
-  matcher: ["/manage/:path*", "/login"],
+  matcher: ["/manage/:path*", "/guest/:path*", "/login"],
 };
